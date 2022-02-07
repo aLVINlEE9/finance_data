bc57331f457201efdeedd37145b8ccf6cdf78218
aLVINlEE9 <fldh3746@gmail.com>
2022-02-07 23:27:08 +0900

    del

--- dart/connect.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy as db
-import dart_fss as dart
-
-class SQLAlchemyConnector:
-	
-	def __init__(self):
-		self.engine = db.create_engine('mysql+pymysql://root:passwd@localhost/db_name')
-		self.connection = self.engine.connect()
-
